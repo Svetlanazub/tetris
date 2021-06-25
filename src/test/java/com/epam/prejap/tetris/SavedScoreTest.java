2d2f603f89f0b1c073b08e68819b90e974254707
Svetlana_Zubkova <svetazub15@gmail.com>
2021-06-25 20:21:13 +0200

    Split SaveScore class to two to extract reading and writing from separate class Make changes in tests Make changes in readSavedScore method

--- src/test/java/com/epam/prejap/tetris/SavedScoreTest.java
+++ src/test/java/com/epam/prejap/tetris/SavedScoreTest.java
@@ -1,63 +1,70 @@
 package com.epam.prejap.tetris;
 
-import org.testng.Assert;
-import org.testng.annotations.*;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
+import com.epam.prejap.tetris.SavedScore;
+//import com.epam.prejap.tetris.SavedScoreIOUtility;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
 
 
 /**
- *  These tests check:
- *  <p><ul>
- *  <li>1. Writing a new line to the file
- *  <li>2. Generating 3 chars
- *  <li>3. toString method for printing 25 lines
- *  <li>4. sorting scores from max to min
- *  </ul><p>
- *  @author Svetlana_Zubkova
+ * These tests check:
+ * <p><ul>
+ * <li>1. Writing a new line to the file
+ * <li>2. Generating 3 chars
+ * <li>3. toString method for printing 25 lines
+ * <li>4. sorting scores from max to min
+ * </ul><p>
+ *
+ * @author Svetlana_Zubkova
  */
-
-@Test (groups =  {"SavedScoreTests"})
 public class SavedScoreTest {
-
     private SavedScore savedScore;
     private String filePath;
 
-    @BeforeClass
+
+    @BeforeClass(groups = {"IntegrationSavedScoreTests"})
     public void setUp() {
         filePath = "resources/test_score_list.json";
         savedScore = new SavedScore(filePath);
     }
 
+    @Test(groups = {"IntegrationSavedScoreTests"})
     public void shouldAddStringToFile() {
-        String name = "AAA";
         int score = 25000;
         savedScore.writeSavedScore(score);
         savedScore = new SavedScore(filePath);
         String test = savedScore.toString();
-        assertTrue(test.contains("AAA 25000"));
+        assertTrue(test.contains("25000"));
     }
 
+    @Test(groups = {"SavedScoreTests"})
     public void shouldGenerateThreeCharsName() {
-        String testName = SavedScoreIOUtility.generateName();
+        String testName = SavedScore.generateName();
         assertEquals(testName.length(), 3);
     }
 
+    @Test(groups = {"IntegrationSavedScoreTests"})
     public void shouldPrint25Lines() {
         String[] sizeOfPrinting = savedScore.toString().split("/n");
         if (sizeOfPrinting.length > 25) {
-            Assert.fail();
+            fail();
         }
     }
 
+    @Test(groups = {"IntegrationSavedScoreTests"})
     public void shouldSortFromMaxScore() {
-        savedScore.writeSavedScore(9900, "AAA");
-        savedScore.writeSavedScore(10000, "BBB");
+        savedScore.writeSavedScore(100000, "BBB");
+        savedScore = null;
         savedScore = new SavedScore(filePath);
         String test = savedScore.toString();
-        if (test.startsWith("BBB 10000")) {
-            Assert.assertTrue(true);
-        }
-        Assert.fail();
+        assertTrue(test.startsWith("BBB 100000"));
+    }
+
+    @AfterClass(groups = {"IntegrationSavedScoreTests"})
+    void tearDown() throws Exception {
+        savedScore = null;
     }
 }
